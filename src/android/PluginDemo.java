d761c9c50e93fa3b8cc896e58d6fc0fe33d36031
eddy <eddy@eddy.local>
2020-10-14 13:21:04 +0800

    toast

--- src/android/PluginDemo.java
+++ src/android/PluginDemo.java
@@ -6,6 +6,7 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.widget.Toast;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -24,6 +25,7 @@ public class PluginDemo extends CordovaPlugin {
 
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
+        Toast.makeText(cordova.GetContext(),message,Toast.LENGTH_SHORT).show();
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
