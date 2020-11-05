ffdcdc789d262712b9ba2054c3e1afb377de14ab
eddy <eddy@eddy.local>
2020-11-05 17:04:27 +0800

    ios

--- src/android/PluginDemo.java
+++ src/android/PluginDemo.java
@@ -7,8 +7,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.widget.Toast;
-
 /**
  * This class echoes a string called from JavaScript.
  */
